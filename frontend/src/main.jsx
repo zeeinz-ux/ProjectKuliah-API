1f8b6d2ca2fbbd13f101d8df01a1edb49e182bfe
zeeinz-ux <zeezeeinz@gmail.com>
2026-04-09 01:42:34 +0700

    add basename for github pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
 );
